d3fc3f62c9ce1a9ed539b6950de48236879312fc
Nathan Shaw <n.shaw@entando.com>
2022-08-26 15:26:07 -0400

    Add conference-details MFE

---
+++ microfrontends/conference-details/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,9 @@
+const conferenceApiGetResponse = {
+  id: 683,
+  name:
+    'Sit harum iusto earum ipsa voluptas id. Vel mollitia ut in ea nobis ex. Fugiat et atque repellendus. Quia id commodi natus quo odio unde et.',
+  location:
+    'Voluptatem cupiditate cum consequuntur. Dolor qui asperiores tempore quo non eos unde illum. Voluptas et repellendus quia voluptatum deserunt. Nulla optio odio quas dolorem qui fuga sit. Occaecati et consectetur esse vero excepturi. Molestiae tempore voluptatem neque est quo et est sapiente.',
+};
+
+export default conferenceApiGetResponse;
