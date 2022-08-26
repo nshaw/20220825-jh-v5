3b46b13a1ffdef226aaadf014d5cd5ad7f7e3640
Nathan Shaw <n.shaw@entando.com>
2022-08-26 15:06:58 -0400

    Rename table-mfe to conference-table, add thumbnail.

--- microfrontends/conference-table/src/custom-elements/ConferenceTableElement.js
+++ microfrontends/conference-table/src/custom-elements/ConferenceTableElement.js
@@ -193,4 +193,4 @@ class ConferenceTableElement extends HTMLElement {
   }
 }
 
-customElements.define('table-mfe', ConferenceTableElement);
+customElements.define('conference-table', ConferenceTableElement);
